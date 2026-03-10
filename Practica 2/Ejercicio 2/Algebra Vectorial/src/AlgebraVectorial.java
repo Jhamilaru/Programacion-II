7897aa78834aeed3a0eaab281219a2acbb8f129f
Jhamilaru <151691520+Jhamilaru@users.noreply.github.com>
2026-03-09 22:50:11 -0400

    Refactor AlgebraVectorial class structure

--- Practica 2/Ejercicio 2/Algebra Vectorial/src/AlgebraVectorial.java
+++ Practica 2/Ejercicio 2/Algebra Vectorial/src/AlgebraVectorial.java
@@ -12,42 +12,27 @@ public class AlgebraVectorial {
         this.bx=bx;
         this.by=by;
     }
-
-    // a,b,c,d) determinar si vectores son perpendiculares
     public boolean perpendicular(){
         return (ax*bx + ay*by)==0;
     }
-
-    // sobrecarga del metodo
     public boolean perpendicular(double ax,double ay,double bx,double by){
         return (ax*bx + ay*by)==0;
     }
-
-    // e,f) determinar si vectores son paralelos
     public boolean paralela(){
         return (ax*by - ay*bx)==0;
     }
-
-    // sobrecarga
     public boolean paralela(double ax,double ay,double bx,double by){
         return (ax*by - ay*bx)==0;
     }
-
-    // g) proyeccion de a sobre b
     public double proyeccion(){
-
         double prod = ax*bx + ay*by;
         double mag = bx*bx + by*by;
-
         return prod/mag;
     }
-
-    // h) componente de a en direccion de b
     public double componente(){
-
         double prod = ax*bx + ay*by;
         double mag = Math.sqrt(bx*bx + by*by);
-
         return prod/mag;
     }
+
 }

30fa142e5d2d63c829fc940d01ef13318f60c0e4
Jhamilaru <151691520+Jhamilaru@users.noreply.github.com>
2026-03-09 22:51:18 -0400

    Refactor Vector3D class structure and methods

--- Practica 2/Ejercicio 3/Vector 3D/src/Vector3D.java
+++ Practica 2/Ejercicio 3/Vector 3D/src/Vector3D.java
@@ -10,55 +10,39 @@ public class Vector3D {
         this.a2=a2;
         this.a3=a3;
     }
-
-    // a) suma de vectores
     public Vector3D suma(Vector3D b){
         return new Vector3D(
         a1+b.a1,
         a2+b.a2,
         a3+b.a3);
     }
-
-    // b) multiplicacion por escalar
     public Vector3D escalar(double r){
         return new Vector3D(
         r*a1,
         r*a2,
         r*a3);
     }
-
-    // c) longitud
     public double longitud(){
         return Math.sqrt(a1*a1 + a2*a2 + a3*a3);
     }
-
-    // d) vector normal
     public Vector3D normal(){
-
         double m = longitud();
-
         return new Vector3D(
         a1/m,
         a2/m,
         a3/m);
     }
-
-    // e) producto escalar
     public double productoEscalar(Vector3D b){
         return a1*b.a1 + a2*b.a2 + a3*b.a3;
     }
-
-    // f) producto vectorial
     public Vector3D productoVectorial(Vector3D b){
-
         double x = a2*b.a3 - a3*b.a2;
         double y = a3*b.a1 - a1*b.a3;
         double z = a1*b.a2 - a2*b.a1;
-
         return new Vector3D(x,y,z);
     }
-
     public void mostrar(){
         System.out.println("("+a1+","+a2+","+a3+")");
     }
+
 }
